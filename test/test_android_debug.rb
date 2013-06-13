7c980e812efcd01a4a3743d628a38e0683a58408
wuntee <mathew.rowley@gmail.com>
2013-06-13 13:07:56 -0400

    Initial commit to android_debug.

---
+++ test/test_android_debug.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestAndroidDebug < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
